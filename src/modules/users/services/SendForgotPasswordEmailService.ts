92bd90c772eed8bb1358dfc491065578f35bc3b9
“phdev” <phdev182@gmail.com>
2021-03-19 11:59:36 -0400

    Configuração do email de recupeção de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UserTokensRepository from '../typeorm/repositories/UsersTokensRepository';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -18,8 +19,13 @@ class SendForgotPasswordEmailService {
       throw new AppError('User dos not exists.');
     }
     console.log(user);
+
     const token = await userTokensRepository.generate(user.id);
-    console.log(token);
+    //console.log(token);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 
